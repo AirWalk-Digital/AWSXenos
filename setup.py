9a483289328c7e426529e3059b3f1456a0718289
costasko <costas.kourmpoglou@airwalkconsulting.com>
2024-05-23 22:55:45 +0100

    bump version and jinja2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 setup(
     name="AWSXenos",
-    version="0.3.0",
+    version="0.4.0",
     author="Costas Kourmpoglou",
     author_email="costas.kourmpoglou@airwalkconsulting.com",
     license="MIT",
